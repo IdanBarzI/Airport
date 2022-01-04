a086d3de47386c046e7073d53f3e41cc872468b8
IdanBarzI <88895210+IdanBarzI@users.noreply.github.com>
2022-01-04 10:22:18 +0200

    Add files via upload

---
+++ FlightCreatorJob/FlightCreatorJob/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FlightCreatorJob
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int i = 1;
+            //var jobThread = new Thread(() =>
+            //{
+            using (HttpClient httpClient = new HttpClient())
+            {
+                while (true)
+                {
+                    await httpClient.GetAsync("http://localhost:5500/api/airport/land/" + i);
+                    //Thread.Sleep(3000);
+                    await Task.Delay(3000);
+                    i++;
+                    await httpClient.GetAsync("http://localhost:5500/api/airport/departure/" + i);
+                    //Thread.Sleep(1200);
+                    await Task.Delay(1200);
+                    i++;
+                }
+            }
+            //});
+            //jobThread.Start();
+        }
+
+    }
+
+}
